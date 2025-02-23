08212210b139c5d46416a7df2fe7b0927d20ad7c
Mohammed Ihthisham NK <mn4009@hw.ac.uk>
2025-02-23 14:13:46 +0000

    initial structure commit

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+ScrollReveal().reveal('.home-container', {
+    delay: 500,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    easing: 'ease-out',
+  });
+  
+  ScrollReveal().reveal('.about-container', {
+    delay: 500,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    easing: 'ease-out',
+  });
+  
+  ScrollReveal().reveal('.projects-container', {
+    delay: 500,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    easing: 'ease-out',
+  });
+  
